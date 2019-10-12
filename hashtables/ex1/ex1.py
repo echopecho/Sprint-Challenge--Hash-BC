1ade0bd042592762be3ad0cf3d539a667a67012c
echopecho <pbsmithtbn@gmail.com>
2019-10-12 11:35:40 -0500

    Complete hashtables ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,9 +1,11 @@
 #  Hint:  You may not need all of these.  Remove the unused functions.
-from hashtables import (HashTable,
-                        hash_table_insert,
-                        hash_table_remove,
-                        hash_table_retrieve,
-                        hash_table_resize)
+from hashtables import (
+    HashTable,
+    hash_table_insert,
+    hash_table_remove,
+    hash_table_retrieve,
+    hash_table_resize,
+)
 
 
 def get_indices_of_item_weights(weights, length, limit):
@@ -12,6 +14,19 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    # result = None
+
+    for i, weight in enumerate(weights):
+        compliment_weight = hash_table_retrieve(ht, limit - weight)
+        hash_table_insert(ht, weight, i)
+        if compliment_weight is not None:
+            result = (
+                (i, compliment_weight)
+                if i >= compliment_weight
+                else (compliment_weight, i)
+            )
+            print(result)
+            return result
 
     return None
 
